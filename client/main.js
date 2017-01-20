262593cf357ae8efdd73bd003ba1c75b2ee07611
Trevor Alfstad <trevor@buildcave.com>
2017-01-19 18:17:36 -0800

    initial react app

---
+++ client/main.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+
+import App from './components/app';
+import reducers from './reducers';
+
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+Meteor.startup(() => {
+  ReactDOM.render(
+    <Provider store={createStoreWithMiddleware(reducers)}>
+      <App />
+    </Provider>
+    , document.querySelector('.container'));
+});
+
+export default createStoreWithMiddleware;
