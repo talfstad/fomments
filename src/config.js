3e42b140f230d980b21e01e99db72675f98073b5
Trevor Alfstad <trevor@buildcave.com>
2017-09-12 21:44:26 -0700

    feat: update config values for production. https://app.asana.com/0/0/427957360691934/f

--- src/config.js
+++ src/config.js
@@ -2,16 +2,16 @@ export default (env) => {
   switch (env) {
     case 'production':
       return ({
-        MARKETING_URL: 'https://fomments.com',
+        MARKETING_URL: 'https://www.fomments.com',
         CDN_ROOT_URL: 'https://cdn.fomments.com',
-        SECTIONS_ROOT_URL: 'https://sections.fomments.com',
+        SECTIONS_ROOT_URL: 'https://www.fomments.com',
         EXTERNAL_MESSAGE_NAMESPACE: 'fomments-external-message',
         EXTERNAL_RESPONSE_NAMESPACE: 'fomments-external-message-response',
       });
     default:
       return ({
         MARKETING_URL: 'https://fomments.com',
-        CDN_ROOT_URL: 'http://localhost:3000',
+        CDN_ROOT_URL: 'http://localhost:8080',
         SECTIONS_ROOT_URL: 'http://localhost:3000',
         EXTERNAL_MESSAGE_NAMESPACE: 'fomments-external-message',
         EXTERNAL_RESPONSE_NAMESPACE: 'fomments-external-message-response',
